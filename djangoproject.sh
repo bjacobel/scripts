1dc2b53b2794d2c095282ad2f8dcd19e0eaf352b
Brian Jacobel <bjacobel@bowdoin.edu>
2013-05-29 12:33:37 -0400

    memcache is deprecated, switched to memcachier

--- djangoproject.sh
+++ djangoproject.sh
@@ -45,10 +45,7 @@ git commit -m "Auto-setup of project skeleton using rdegges/django-skel"
 
 echo -e "\x1B[1;31m>>>> Prepping for Heroku production testing... \x1B[0m"
 heroku create $PROJECTNAME
-#heroku wants a credit card to be able to add any addons... fake-ass bitche$$
-#heroku addons:add cloudamqp:lemur heroku-postgresql:dev scheduler:standard memcache:5mb newrelic:standard pgbackups:auto-month sentry:developer
-#just do postgresql for now
-heroku addons:add heroku-postgresql:dev
+heroku addons:add cloudamqp:lemur heroku-postgresql:dev memcachier:dev scheduler:standard newrelic:standard pgbackups:auto-month sentry:developer
 echo -e "\x1B[1;31m>>>> Pushing to Heroku and installing requirements...\x1B[0m"
 git push heroku master
 heroku pg:info
@@ -56,7 +53,7 @@ echo -e "\x1B[1;31m>>>> Type in the name of the database Heroku created above: \
 read -p "Database name?: " db
 heroku pg:promote $db
 heroku config:add DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings.prod
-#technically pseudorandom, but damn rear random enough
+#technically pseudorandom, but damn near random enough
 SECRETKEY = cat /dev/urandom|LANG=C tr -dc "a-zA-Z0-9-_\$\?"|fold -w 40|head -1
 heroku config:add SECRET_KEY=$SECRETKEY
 
