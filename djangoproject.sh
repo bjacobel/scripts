945b8b64c239c20690ef218c2706652ad2e6e417
Brian Jacobel <bjacobel@bowdoin.edu>
2013-05-29 11:08:27 -0400

    think I fixed the heroku Cedar deploy errors? Should test from scratch to make sure

--- djangoproject.sh
+++ djangoproject.sh
@@ -2,7 +2,7 @@
 
 PROJECTNAME=$1
 
-echo -e "\x1B[1;31m>>>>Setting up a virtual environment... \x1B[0m"
+echo -e "\x1B[1;31m>>>> Setting up a virtual environment... \x1B[0m"
 export WORKON_HOME=~/Envs
 mkdir -p $WORKON_HOME
 source /usr/local/bin/virtualenvwrapper.sh
@@ -10,21 +10,21 @@ mkvirtualenv $PROJECTNAME-env
 workon $PROJECTNAME-env
 add2virtualenv 'pwd'
 
-echo -e "\x1B[1;31m>>>>Installing the latest Django... \x1B[0m"
+echo -e "\x1B[1;31m>>>> Installing the latest Django... \x1B[0m"
 pip install django
 
 #start the project using rdegges' django skeleton
-echo -e "\x1B[1;31m>>>>Creating a project from the skeleton... \x1B[0m"
+echo -e "\x1B[1;31m>>>> Creating a project from the skeleton... \x1B[0m"
 django-admin.py startproject --template=https://github.com/rdegges/django-skel/zipball/master $PROJECTNAME
 
 #install all the requirements of that skeleton (there are a lot)
 cd $PROJECTNAME
-echo -e "\x1B[1;31m>>>>Installing requirements...\x1B[0m"
+echo -e "\x1B[1;31m>>>> Installing requirements...\x1B[0m"
 pip install -r reqs/dev.txt
 
 #postgresql needs to be installed frm src, pip's version is broken on OSX
 #download the most recent from github
-curl https://codeload.github.com/psycopg/psycopg2/zip/master > psycopg2.zip
+curl https://codeload.github.com/psycopg/psycopg2/zip/master > psycopg2.zip #OSX doesn't have wget???
 unzip -q psycopg2.zip
 cd psycopg2-master
 easy_install .
@@ -33,26 +33,26 @@ rm -rf psycopg2*
 
 chmod u+x ./manage.py
 
-echo -e "\x1B[1;31m>>>>Setting up your database... \x1B[0m"
-echo -e "\x1B[1;31m>>>>You'll need to provide a username/password for an admin account. \x1B[0m"
+echo -e "\x1B[1;31m>>>> Setting up your database... \x1B[0m"
+echo -e "\x1B[1;31m>>>> You'll need to provide a username/password for an admin account. \x1B[0m"
 ./manage.py syncdb
 ./manage.py migrate
 
-echo -e "\x1B[1;31m>>>>Version controlling with Git... \x1B[0m"
-cd $PROJECTNAME
+echo -e "\x1B[1;31m>>>> Version controlling with Git... \x1B[0m"
 git init
 git add .
 git commit -m "Auto-setup of project skeleton using rdegges/django-skel"
 
-echo -e "\x1B[1;31m>>>>Prepping for Heroku production testing... \x1B[0m"
+echo -e "\x1B[1;31m>>>> Prepping for Heroku production testing... \x1B[0m"
 heroku create $PROJECTNAME
 #heroku wants a credit card to be able to add any addons... fake-ass bitche$$
 #heroku addons:add cloudamqp:lemur heroku-postgresql:dev scheduler:standard memcache:5mb newrelic:standard pgbackups:auto-month sentry:developer
 #just do postgresql for now
 heroku addons:add heroku-postgresql:dev
+echo -e "\x1B[1;31m>>>> Pushing to Heroku and installing requirements...\x1B[0m"
 git push heroku master
 heroku pg:info
-echo -e "\x1B[1;31m>>>>Type in the name of the database Heroku created above: \x1B[0m"
+echo -e "\x1B[1;31m>>>> Type in the name of the database Heroku created above: \x1B[0m"
 read -p "Database name?: " db
 heroku pg:promote $db
 heroku config:add DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings.prod
@@ -60,10 +60,8 @@ heroku config:add DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings.prod
 SECRETKEY = cat /dev/urandom|LANG=C tr -dc "a-zA-Z0-9-_\$\?"|fold -w 40|head -1
 heroku config:add SECRET_KEY=$SECRETKEY
 
-message1="\x1B[1;31m>>>>All done! \nTo test locally: \t./manage.py runserver \nTo deploy to Heroku: \tgit push heroku master && heroku ps:scale web=1, heroku ps, heroku open\nTo add to GitHub: \tgit remote add git@github.com:bjacobel/"
+message1="\x1B[1;31m>>>> All done! \nTo test locally: \t./manage.py runserver \nTo run on Heroku: \theroku ps:scale web=1, heroku ps, heroku open\nTo add to GitHub: \tgit remote add git@github.com:bjacobel/"
 message2=".git\x1B[0m"
 message=$message1$PROJECTNAME$message2
 
 echo -e $message
-
-#is git broken?
