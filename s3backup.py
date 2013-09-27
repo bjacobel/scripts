74e4d04d95fe79fefed7fc549a7b43248397b131
Brian Jacobel <bjacobel@gmail.com>
2013-09-26 23:17:54 -0400

    Back up ALL the things. potentially sometime investigate how to create a bucket?

--- s3backup.py
+++ s3backup.py
@@ -6,14 +6,29 @@ import subprocess
 import boto
 import os
 import sys
-from getopt import getopt
+import getopt
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"b:",["bucket="])
+except getopt.GetoptError:
+    print 's3backup.py [-b <s3 bucket>]'
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == "-b":
+        BUCKET = arg
 
 ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-SECRET_KEY = os.environ.get('AWS_SECRET_ID')
+SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 
 if ACCESS_KEY_ID is None:
     ACCESS_KEY_ID = raw_input('Your access key ID: ')
 if SECRET_KEY is None:
     SECRET_KEY = raw_input('Your secret key: ')
 
-backup_command = "boto-rsync -w -a {access} -s {secret} s3://{bucket}/ ./".format(access=ACCESS_KEY_ID, secret=SECRET_KEY, bucket=sys.argv[1])
+try:
+    print "Backing up to bucket {}".format(BUCKET_
+except:
+    BUCKET = os.path.basename(os.path.abspath(__file__))
+
+backup_command = "boto-rsync -w -a {access} -s {secret} s3://{bucket}/ ./".format(access=ACCESS_KEY_ID, secret=SECRET_KEY, bucket=BUCKET)
