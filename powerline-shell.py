1087d0437957d153fc005f71900add0f19731712
Brian Jacobel <bjacobel@gmail.com>
2014-01-22 14:54:04 -0500

    Tried to get Powerline to work with RVM, didn't work, gave up.

--- powerline-shell.py
+++ powerline-shell.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 import sys
+import subprocess
 
 def warn(msg):
     print '[powerline-bash] ', msg
@@ -11,14 +12,20 @@ def warn(msg):
 class Powerline:
     symbols = {
         'compatible': {
+            'lock': 'RO',
+            'network': 'SSH',
             'separator': u'\u25B6',
             'separator_thin': u'\u276F'
         },
         'patched': {
+            'lock': u'\uE0A2',
+            'network': u'\uE0A2',
             'separator': u'\uE0B0',
             'separator_thin': u'\uE0B1'
         },
         'flat': {
+            'lock': '',
+            'network': '',
             'separator': '',
             'separator_thin': ''
         },
@@ -36,6 +43,8 @@ class Powerline:
         mode, shell = args.mode, args.shell
         self.color_template = self.color_templates[shell]
         self.reset = self.color_template % '[0m'
+        self.lock = Powerline.symbols[mode]['lock']
+        self.network = Powerline.symbols[mode]['network']
         self.separator = Powerline.symbols[mode]['separator']
         self.separator_thin = Powerline.symbols[mode]['separator_thin']
         self.segments = []
@@ -114,22 +123,35 @@ if __name__ == "__main__":
     powerline = Powerline(args, get_valid_cwd())
 
 
-class Color:
-    USERNAME_FG = 19  # dark blue
+class DefaultColor:
+    """
+    This class should have the default colors for every segment.
+    Please test every new segment with this theme first.
+    """
+    USERNAME_FG = 19
     USERNAME_BG = 240
 
-    HOSTNAME_FG = 75  # light blue
+    HOSTNAME_FG = 75
     HOSTNAME_BG = 238
 
+    HOME_SPECIAL_DISPLAY = True
+    HOME_BG = 31  # blueish
+    HOME_FG = 15  # white
     PATH_BG = 237  # dark grey
     PATH_FG = 250  # light grey
     CWD_FG = 254  # nearly-white grey
     SEPARATOR_FG = 244
 
+    READONLY_BG = 124
+    READONLY_FG = 254
+
+    SSH_BG = 166 # medium orange
+    SSH_FG = 254
+
     REPO_CLEAN_BG = 118  # a light green color
     REPO_CLEAN_FG = 0  # black
     REPO_DIRTY_BG = 208  # orange
-    REPO_DIRTY_FG = 0  # black
+    REPO_DIRTY_FG = 15  # white
 
     JOBS_FG = 39
     JOBS_BG = 238
@@ -145,8 +167,11 @@ class Color:
     VIRTUAL_ENV_BG = 35  # a mid-tone green
     VIRTUAL_ENV_FG = 00
 
-
-import os
+class Color(DefaultColor):
+    """
+    This subclass is required when the user chooses to use 'default' theme.
+    Because the segments require a 'Color' class for every theme.
+    """
 
 def add_virtual_env_segment():
     env = os.getenv('VIRTUAL_ENV')
@@ -161,19 +186,20 @@ def add_virtual_env_segment():
 add_virtual_env_segment()
 
 
+def add_ruby_version_segment():
+    try:
+        p1 = subprocess.Popen(["ruby", "-v"], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["sed", "s/ (.*//"], stdin=p1.stdout, stdout=subprocess.PIPE)
+        version = p2.communicate()[0].rstrip()
+        if os.environ.has_key("GEM_HOME"):
+            gem = os.environ["GEM_HOME"].split("@")
+            if len(gem) > 1:
+                version += " " + gem[1]
+                powerline.append(version, 15, 1)
+    except OSError:
+        return
 
-def add_username_segment():
-    if powerline.args.shell == 'bash':
-        user_prompt = ' \\u'
-    elif powerline.args.shell == 'zsh':
-        user_prompt = ' %n'
-    else:
-        import os
-        user_prompt = ' %s' % os.getenv('USER')
-
-    powerline.append(user_prompt, Color.USERNAME_FG, Color.USERNAME_BG)
-
-add_username_segment()
+add_ruby_version_segment()
 
 
 def add_hostname_segment():
@@ -189,12 +215,12 @@ def add_hostname_segment():
         powerline.append(host_prompt, FG, BG)
     else:
         if powerline.args.shell == 'bash':
-            host_prompt = ' \\h'
+            host_prompt = ' \\h '
         elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m'
+            host_prompt = ' %m '
         else:
             import socket
-            host_prompt = ' %s' % socket.gethostname().split('.')[0]
+            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
 
         powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
 
@@ -202,7 +228,13 @@ def add_hostname_segment():
 add_hostname_segment()
 
 
-import os
+def add_ssh_segment():
+
+    if os.getenv('SSH_CLIENT'):
+        powerline.append(' %s ' % powerline.network, Color.SSH_FG, Color.SSH_BG)
+
+add_ssh_segment()
+
 
 def get_short_path(cwd):
     home = os.getenv('HOME')
@@ -213,8 +245,9 @@ def get_short_path(cwd):
         path += os.sep + names[i]
         if os.path.samefile(path, home):
             return ['~'] + names[i+1:]
+    if not names[0]:
+        return ['/']
     return names
-
 
 def add_cwd_segment():
     cwd = powerline.cwd or os.getenv('PWD')
@@ -226,191 +259,18 @@ def add_cwd_segment():
 
     if not powerline.args.cwd_only:
         for n in names[:-1]:
-            powerline.append(' %s ' % n, Color.PATH_FG, Color.PATH_BG,
+            if n == '~' and Color.HOME_SPECIAL_DISPLAY:
+                powerline.append(' %s ' % n, Color.HOME_FG, Color.HOME_BG)
+            else:
+                powerline.append(' %s ' % n, Color.PATH_FG, Color.PATH_BG,
                     powerline.separator_thin, Color.SEPARATOR_FG)
-    powerline.append(' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG)
+
+    if names[-1] == '~' and Color.HOME_SPECIAL_DISPLAY:
+        powerline.append(' %s ' % names[-1], Color.HOME_FG, Color.HOME_BG)
+    else:
+        powerline.append(' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG)
 
 add_cwd_segment()
-
-
-import re
-import subprocess
-
-def get_git_status():
-    has_pending_commits = True
-    has_untracked_files = False
-    origin_position = ""
-    output = subprocess.Popen(['git', 'status', '--ignore-submodules'],
-            stdout=subprocess.PIPE).communicate()[0]
-    for line in output.split('\n'):
-        origin_status = re.findall(
-                r"Your branch is (ahead|behind).*?(\d+) comm", line)
-        if origin_status:
-            origin_position = " %d" % int(origin_status[0][1])
-            if origin_status[0][0] == 'behind':
-                origin_position += u'\u21E3'
-            if origin_status[0][0] == 'ahead':
-                origin_position += u'\u21E1'
-
-        if line.find('nothing to commit') >= 0:
-            has_pending_commits = False
-        if line.find('Untracked files') >= 0:
-            has_untracked_files = True
-    return has_pending_commits, has_untracked_files, origin_position
-
-
-def add_git_segment():
-    #cmd = "git branch 2> /dev/null | grep -e '\\*'"
-    p1 = subprocess.Popen(['git', 'branch', '--no-color'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p2 = subprocess.Popen(['grep', '-e', '\\*'], stdin=p1.stdout, stdout=subprocess.PIPE)
-    output = p2.communicate()[0].strip()
-    if not output:
-        return
-
-    branch = output.rstrip()[2:]
-    has_pending_commits, has_untracked_files, origin_position = get_git_status()
-    branch += origin_position
-    if has_untracked_files:
-        branch += ' +'
-
-    bg = Color.REPO_CLEAN_BG
-    fg = Color.REPO_CLEAN_FG
-    if has_pending_commits:
-        bg = Color.REPO_DIRTY_BG
-        fg = Color.REPO_DIRTY_FG
-
-    powerline.append(' %s ' % branch, fg, bg)
-
-try:
-    add_git_segment()
-except OSError:
-    pass
-except subprocess.CalledProcessError:
-    pass
-
-
-import os
-import subprocess
-
-def get_hg_status():
-    has_modified_files = False
-    has_untracked_files = False
-    has_missing_files = False
-    output = subprocess.Popen(['hg', 'status'],
-            stdout=subprocess.PIPE).communicate()[0]
-    for line in output.split('\n'):
-        if line == '':
-            continue
-        elif line[0] == '?':
-            has_untracked_files = True
-        elif line[0] == '!':
-            has_missing_files = True
-        else:
-            has_modified_files = True
-    return has_modified_files, has_untracked_files, has_missing_files
-
-def add_hg_segment():
-    branch = os.popen('hg branch 2> /dev/null').read().rstrip()
-    if len(branch) == 0:
-        return False
-    bg = Color.REPO_CLEAN_BG
-    fg = Color.REPO_CLEAN_FG
-    has_modified_files, has_untracked_files, has_missing_files = get_hg_status()
-    if has_modified_files or has_untracked_files or has_missing_files:
-        bg = Color.REPO_DIRTY_BG
-        fg = Color.REPO_DIRTY_FG
-        extra = ''
-        if has_untracked_files:
-            extra += '+'
-        if has_missing_files:
-            extra += '!'
-        branch += (' ' + extra if extra != '' else '')
-    return powerline.append(' %s ' % branch, fg, bg)
-
-add_hg_segment()
-
-
-import subprocess
-
-def add_svn_segment():
-    is_svn = subprocess.Popen(['svn', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    is_svn_output = is_svn.communicate()[1].strip()
-    if len(is_svn_output) != 0:
-        return
-
-    #"svn status | grep -c "^[ACDIMRX\\!\\~]"
-    p1 = subprocess.Popen(['svn', 'status'], stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    p2 = subprocess.Popen(['grep', '-c', '^[ACDIMR\\!\\~]'],
-            stdin=p1.stdout, stdout=subprocess.PIPE)
-    output = p2.communicate()[0].strip()
-    if len(output) > 0 and int(output) > 0:
-        changes = output.strip()
-        powerline.append(' %s ' % changes, Color.SVN_CHANGES_FG, Color.SVN_CHANGES_BG)
-
-try:
-    add_svn_segment()
-except OSError:
-    pass
-except subprocess.CalledProcessError:
-    pass
-
-
-import os
-import subprocess
-
-def get_fossil_status():
-    has_modified_files = False
-    has_untracked_files = False
-    has_missing_files = False
-    output = os.popen('fossil changes 2>/dev/null').read().strip()
-    has_untracked_files = True if os.popen("fossil extras 2>/dev/null").read().strip() else False
-    has_missing_files = 'MISSING' in output
-    has_modified_files = 'EDITED' in output
-
-    return has_modified_files, has_untracked_files, has_missing_files
-
-def add_fossil_segment():
-    subprocess.Popen(['fossil'], stdout=subprocess.PIPE).communicate()[0]
-    branch = ''.join([i.replace('*','').strip() for i in os.popen("fossil branch 2> /dev/null").read().strip().split("\n") if i.startswith('*')])
-    if len(branch) == 0:
-        return
-
-    bg = Color.REPO_CLEAN_BG
-    fg = Color.REPO_CLEAN_FG
-    has_modified_files, has_untracked_files, has_missing_files = get_fossil_status()
-    if has_modified_files or has_untracked_files or has_missing_files:
-        bg = Color.REPO_DIRTY_BG
-        fg = Color.REPO_DIRTY_FG
-        extra = ''
-        if has_untracked_files:
-            extra += '+'
-        if has_missing_files:
-            extra += '!'
-        branch += (' ' + extra if extra != '' else '')
-    powerline.append(' %s ' % branch, fg, bg)
-
-try:
-    add_fossil_segment()
-except OSError:
-    pass
-except subprocess.CalledProcessError:
-    pass
-
-
-import os
-import re
-import subprocess
-
-def add_jobs_segment():
-    ppid = os.getppid()
-    output = subprocess.Popen(['ps', '-a', '-o', 'ppid'], stdout=subprocess.PIPE).communicate()[0]
-    num_jobs = len(re.findall(str(ppid), output)) - 1
-
-    if num_jobs > 0:
-        powerline.append(' %d ' % num_jobs, Color.JOBS_FG, Color.JOBS_BG)
-
-add_jobs_segment()
 
 
 def add_root_indicator_segment():
@@ -429,4 +289,4 @@ def add_root_indicator_segment():
 add_root_indicator_segment()
 
 
-sys.stdout.write(powerline.draw()+" ")
+sys.stdout.write(powerline.draw())
